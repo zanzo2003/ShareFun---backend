7dd27a134c4002a638a11cb59a516f2e5d750cc4
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2023-11-25 15:01:47 +0530

    cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 const connectDB = require('./db');
 connectDB();
